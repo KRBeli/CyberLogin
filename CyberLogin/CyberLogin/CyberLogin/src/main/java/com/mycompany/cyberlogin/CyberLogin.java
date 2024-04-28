de77f57f715acbe040b49dff10843818e80f6c40
Lucas Ievenes Ratti <61846972+Ievenes@users.noreply.github.com>
2024-04-28 17:53:40 -0300

    teste

---
+++ CyberLogin/CyberLogin/CyberLogin/src/main/java/com/mycompany/cyberlogin/CyberLogin.java
@@ -0,0 +1,14 @@
+package com.mycompany.cyberlogin;
+
+public class CyberLogin {
+
+    public static void main(String[] args) {
+               /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                
+                new CyberLoginTela().setVisible(true);
+            }
+        });
+    }
+}]
